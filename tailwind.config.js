0721ffda1623ad2f8a725187ccc149ab7d5c65d0
Cirox <ciroxit@gmail.com>
2020-10-15 16:16:32 +0200

    Up

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
   purge: [],
   theme: {
     extend: {
+      colors: {
+        primary: '#236EFF',
+        secondary: '#652EC0',
+        grai:'#D9E0E4',
+        text:'#212429',
+        // ...
+      },
       gridTemplateColumns: {
         'auto': 'repeat(auto-fit, minmax(280px, 1fr))',
       }
